7a71acf8992a29dd648e86e10a251ac7e73fd86d
Your Name <you@example.com>
2023-08-21 05:15:16 +0000

    コミットメッセージ

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import IndexView
+from .views import AboutView
+
+
+urlpatterns = [
+    path('',IndexView.as_view()),
+    path('about/',AboutView.as_view()),
+]
